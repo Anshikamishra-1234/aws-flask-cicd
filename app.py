97bee47cdf41dc93652bd7879eb516cf6e30aea0
Anshika Mishra <anshikamishra5479@gmail.com>
2026-09-19 16:26:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! AWS CI/CD Project is Working."
+    return "Hello! AWS CI/CD Project is Working v2."
 
 @app.route("/health")
 def health():
